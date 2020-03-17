d6f011c7f1eb614fecae8c7744d64c3fcf9fee7c
unknown <sanchezdanielacv@gmail.com>
2020-03-16 23:56:11 -0300

    se agregan propiedades de las donaciones

--- src/main/java/com/employee/donations/service/http/DonationRequest.java
+++ src/main/java/com/employee/donations/service/http/DonationRequest.java
@@ -9,13 +9,23 @@ import lombok.*;
 @NoArgsConstructor
 public class DonationRequest {
 
+    private String name;
+    private String lastName;
+    private String dni;
+    private String dateBirth;
+    private String gender;
+    private String mobile;
+    private String phone;
+    private String address;
+    private String city;
+    private String province;
     private Float amount;
+    private String cardNumber;
+    private String cardholder;
+    private String dniCardholder;
+    private String expirationDate;
+    private String observation;
     private Integer payType;
     private Integer idEmployee;
     private Integer idCampaign;
-    private String dni;
-    private String name;
-    private String lastName;
-    private Integer age;
-    private String gender;
 }
